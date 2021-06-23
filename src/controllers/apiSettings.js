5ce9b60770a431626628855b563e55c3487b9d74
Egor <bifrost.step@gmail.com>
2021-06-23 16:03:17 +0300

    Теперь при клонировании учитывается mainBranch

--- src/controllers/apiSettings.js
+++ src/controllers/apiSettings.js
@@ -3,6 +3,7 @@ const gitCloneRepo = require('../utils/childProcesses/gitCloneRepo');
 const deleteSavedStructures = require('../utils/deleteSavedStructures');
 
 let lastRepoName = '';
+let lastMainBranch = '';
 
 const getSettings = async (_, res) => {
   const response = await RepositoryDAL.getRepositorySettings();
@@ -10,8 +11,14 @@ const getSettings = async (_, res) => {
 };
 
 const sendSettings = async (req, res) => {
-  if (lastRepoName !== req.body.repoName) await gitCloneRepo(req.body.repoName);
+  if (
+    lastRepoName !== req.body.repoName ||
+    lastMainBranch !== req.body.mainBranch
+  ) {
+    await gitCloneRepo(req.body.repoName, req.body.mainBranch);
+  }
   lastRepoName = req.body.repoName;
+  lastMainBranch = req.body.mainBranch;
   const response = await RepositoryDAL.sendRepositorySettings(req.body);
   res.send(response);
 };
--- src/utils/childProcesses/gitCloneRepo.js
+++ src/utils/childProcesses/gitCloneRepo.js
@@ -3,10 +3,10 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const deleteSavedStructures = require('../deleteSavedStructures');
 
-module.exports = async (repoName) => {
+module.exports = async (repoName, mainBranch) => {
   await deleteSavedStructures.deleteSavedRepository();
   console.time('cloning');
-  const err = await exec(`git clone ${repoName} ./data/Repository`);
+  const err = await exec(`git clone --branch=${mainBranch} ${repoName} ./data/Repository`);
   console.timeEnd('cloning');
   if (err) {
     return {status: 500, error: err};
